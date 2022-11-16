3679ab2e93bd9256020b28e583fb2c5ca43b50ec
Peter <peterjclim@yahoo.com>
2022-11-16 16:30:59 +0800

    New Addtion

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,26 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
+
+def portfolio_cost(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    total_cost = 0.0
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            nshares = int(row[1])
+            price = float(row[2])
+            total_cost += nshares * price
+    return total_cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'G:\GettingStarted_Can_Delete\practical-python\Work\Data\portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,26 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+import sys
+
+def portfolio_cost(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+
+    return portfolio
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'G:\GettingStarted_Can_Delete\practical-python\Work\Data\portfolio.csv'
+
+out = portfolio_cost(filename)
+print('Portfolio Out:', out)
